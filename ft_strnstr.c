02dc5bad7f1c9b8352fe915a6c6312c28ef26c7a
Magomed Isaev <misaev@c1r8p2.42nice.fr>
2021-04-05 12:34:56 +0200

    TESTE OK

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: misaev <misaev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 11:34:51 by misaev            #+#    #+#             */
-/*   Updated: 2021/03/31 18:19:00 by misaev           ###   ########.fr       */
+/*   Updated: 2021/04/05 12:32:33 by misaev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (big[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (big[i + j] == little[j])
+		while (big[i + j] == little[j] && i + j < len)
 		{
 			if (little[j + 1] == '\0')
 				return ((char *)big + i);
