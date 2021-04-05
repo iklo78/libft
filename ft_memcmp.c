24e2b9d6ac625016dea48e4698ad0aec42ac206a
Magomed Isaev <misaev@c1r8p2.42nice.fr>
2021-04-05 12:12:08 +0200

    ft_memcmp all = OK

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: misaev <misaev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 18:06:51 by misaev            #+#    #+#             */
-/*   Updated: 2021/03/31 17:56:47 by misaev           ###   ########.fr       */
+/*   Updated: 2021/04/05 11:54:49 by misaev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ int	ft_memcmp( const void *pter1, const void *pter2, size_t size )
 {
 	unsigned char	*ptr1;
 	unsigned char	*ptr2;
-	size_t			i;
+	size_t i;
 
+	i = 0;
 	ptr1 = (unsigned char *)pter1;
 	ptr2 = (unsigned char *)pter2;
-	i = 0;
-	while (ptr1[i] && ptr2[i] && i < size)
+	while (size > i)
 	{
-		if (ptr1[i] > ptr2[i])
-			return (1);
-		else if (ptr1[i] < ptr2[i])
-			return (-1);
-		i++;
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		ptr1++;
+		ptr2++;
+		size--;
 	}
 	return (0);
 }
