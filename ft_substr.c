4d772a6b5a4fca9f982369d3bc656a94daa1e509
Magomed Isaev <misaev@c1r8p2.42nice.fr>
2021-04-05 14:49:35 +0200

    TEST OK

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: misaev <misaev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 15:40:01 by misaev            #+#    #+#             */
-/*   Updated: 2021/03/31 18:21:03 by misaev           ###   ########.fr       */
+/*   Updated: 2021/04/05 14:48:24 by misaev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	b = start;
+	if (start > len)
+		return (ft_strdup(""));
 	while (s[b] && i < len)
 	{
 		b++;
 		i++;
 	}
 	new = malloc(sizeof(char) * i);
+	if (!new)
+		return (NULL);
 	i = 0;
 	while (s[start] && i < len)
 	{
@@ -33,7 +37,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		i++;
 		start++;
 	}
-	if (new == NULL)
-		return (NULL);
+	new[i] = '\0';
 	return (new);
 }
