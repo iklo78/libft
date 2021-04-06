79264928fec3b23725dab2c28abefc5f45093c48
Magomed Isaev <misaev@c1r8p2.42nice.fr>
2021-04-06 13:14:02 +0200

    split avec moin de 25 ligne

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: misaev <misaev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 11:19:03 by misaev            #+#    #+#             */
-/*   Updated: 2021/04/06 12:33:35 by misaev           ###   ########.fr       */
+/*   Updated: 2021/04/06 13:11:22 by misaev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static char		*ft_strncpy(char *dest, char *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dest || !src)
+		return NULL;
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
@@ -34,7 +36,8 @@ size_t	nbr_word(const char *s, char sep)
 {
 	size_t i;
 	size_t len;
-
+	if (!s)
+		return 0;
 	i = 0;
 	len = 0;
 	while(s[i] != '\0')
@@ -45,37 +48,37 @@ size_t	nbr_word(const char *s, char sep)
 		}
 	return (len + 1);
 }
-
-char	**ft_split(const char *s, char sep)
+char	**Cool(const char *s, char sep)
 {
 	size_t i;
 	size_t len_mot;
 	size_t m_dest;
 	char **dest;
 
-	if (!s)
-		return (NULL);
-	m_dest = 0;
 	dest = ft_calloc(sizeof(char **), nbr_word(s, sep) + 1);
 	if (!dest)
-		return (NULL);
-	i = 0;
-	while(s[i])
+		return NULL;
+	m_dest = 0;
+	i = -1;
+	while(s[++i])
 	{
 		len_mot = 0;
 		if(s[i] != sep)
-        {
-            while(s[i + len_mot] != sep && s[i + len_mot] != '\0')
-                len_mot++;
-            dest[m_dest] = ft_calloc(sizeof(char), (len_mot + 1));
-			if (!dest)
-				return (NULL);
-            ft_strncpy(dest[m_dest], (char *)&s[i], len_mot);
-            m_dest++;
-            i = i + len_mot - 1;
-        }
-        i++;
-    }
-    dest[m_dest] = 0;
-    return (dest);
+		{
+			while(s[i + len_mot] != sep && s[i + len_mot] != '\0')
+				len_mot++;
+			dest[m_dest] = ft_calloc(sizeof(char), (len_mot + 1));
+			ft_strncpy(dest[m_dest], (char *)&s[i], len_mot);
+			m_dest++;
+			i = i + len_mot - 1;
+		}
+	}
+	dest[m_dest] = 0;
+	return dest;
+}
+char	**ft_split(const char *s, char sep)
+{
+	if (!s)
+		return (NULL);
+	return Cool(s,sep);
 }
