7951bd7054d0e44942cc3fcf86cc69dbd92f8a2b
Magomed Isaev <misaev@c2r9p3.42nice.fr>
2021-04-08 11:05:53 +0200

    ALL TEST OK NORM OK (voir avec youcef pour confirmer)

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: misaev <misaev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 11:22:56 by misaev            #+#    #+#             */
-/*   Updated: 2021/04/07 18:21:57 by misaev           ###   ########.fr       */
+/*   Updated: 2021/04/08 09:08:18 by misaev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (lst != NULL && lst -> content != NULL)
-	{
-		del((lst)->content);
-		free((void *) lst);
-	}
+	if (!lst || !del)
+		return ;
+	(*del)(lst->content);
+	free(lst);
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: misaev <misaev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 16:37:38 by misaev            #+#    #+#             */
-/*   Updated: 2021/04/07 18:22:26 by misaev           ###   ########.fr       */
+/*   Updated: 2021/04/08 11:01:02 by misaev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
+	t_list	*p_courant;
+	t_list	*p_courant2;
 
-	ft_lstiter(lst -> next, lst -> content);
-	new = malloc(sizeof(ft_lstnew(f(lst))));
-	if (!new)
+	p_courant2 = NULL;
+	if (!lst)
 		return (NULL);
-	if (new != NULL && new -> content != NULL)
+	while (lst)
 	{
-		del ((new)->content);
-		free((void *)new);
+		p_courant = ft_lstnew(lst->content);
+		p_courant->content = (f)(lst->content);
+		if (!p_courant)
+			ft_lstdelone(p_courant, del);
+		ft_lstadd_back(&p_courant2, p_courant);
+		lst = lst->next;
 	}
-	return (new);
+	return (p_courant2);
 }
